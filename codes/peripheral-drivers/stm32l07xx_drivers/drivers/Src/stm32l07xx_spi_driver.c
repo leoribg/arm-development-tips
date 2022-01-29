20391253f36c62a95d6ff3eae8c8f9d824bbbbf8
leoribg <leonardo.ribeiro@gee.inatel.br>
2022-01-29 11:59:44 -0300

    Implement SPI_Init function

--- codes/peripheral-drivers/stm32l07xx_drivers/drivers/Src/stm32l07xx_spi_driver.c
+++ codes/peripheral-drivers/stm32l07xx_drivers/drivers/Src/stm32l07xx_spi_driver.c
@@ -43,7 +43,47 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t En) {
  * @Note              -
  */
 void SPI_Init(SPI_Handle_t *pSPIHandle) {
+	uint32_t temp = 0;
 
+	/* peripheral clock enable */
+	SPI_PeriClockControl(pSPIHandle->pSPIx, ENABLE);
+
+	/* Configure the SPI CR1 Register */
+	/* SPI_DeviceMode */
+	temp  = pSPIHandle->SPI_Config.SPI_DeviceMode << SPI_CR1_MSTR_BIT;
+
+	/* SPI_BusConfig */
+	if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_FD) {
+		/* Clear BIDI MODE*/
+		temp &= ~( 1 << SPI_CR1_BIDIMODE);
+	}
+	else if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_HD) {
+		/* Set BIDI MODE*/
+		temp |= ( 1 << SPI_CR1_BIDIMODE);
+	}
+	else if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY) {
+		/* Clear BIDI MODE*/
+		temp &= ~( 1 << SPI_CR1_BIDIMODE);
+		/* Set RXONLY */
+		temp |= ( 1 << SPI_CR1_RXONLY);
+	}
+
+	/* SPI_SclkSpeed */
+	temp |= pSPIHandle->SPI_Config.SPI_SclkSpeed << SPI_CR1_BR;
+
+	/* SPI_DFF */
+	temp |= pSPIHandle->SPI_Config.SPI_DFF << SPI_CR1_DFF;
+
+	/* SPI_CPOL */
+	temp |= pSPIHandle->SPI_Config.SPI_CPOL << SPI_CR1_CPOL;
+
+	/* SPI_CPHA */
+	temp |= pSPIHandle->SPI_Config.SPI_CPHA << SPI_CR1_CPHA;
+
+	/* SPI_SSM */
+	temp |= pSPIHandle->SPI_Config.SPI_SSM << SPI_CR1_SSM;
+
+	pSPIHandle->pSPIx->CR1 = temp;
 }
 
 void SPI_DeInit(SPI_RegDef_t *pSPIx) {
